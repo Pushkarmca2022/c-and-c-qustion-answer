2d42afffd748f3f9c6172bec3b753d2fe165d39f
pushkar <pushkar@gmail.com>
2021-10-14 11:06:18 +0530

    vector program

---
+++ vector.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+//#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    vector<int> v1(5,10);
+    for(int i=0;i<=4;i++)
+    cout<<v1[i]<<endl;
+cout<<v1.capacity();
+cout<<v1.size();
+
+}
